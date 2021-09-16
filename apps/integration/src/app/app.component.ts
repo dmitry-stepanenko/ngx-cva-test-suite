8a8f289b188d367df3ec2ce924f2434257e8de48
Dmitriy Stepanenko <stepanenkodmitri@gmail.com>
2021-09-16 17:47:11 +0300

    initial commit

---
+++ apps/integration/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-cva-test-suite-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'integration';
+}
